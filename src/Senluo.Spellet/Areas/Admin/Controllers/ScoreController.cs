8b4dc6b69589323f31dc308920ccabef8d9f48e5
tianzhiyuan <tianzhiyuan126@126.com>
2014-09-11 20:24:20 +0800

    修改Master Layout

---
+++ src/Senluo.Spellet/Areas/Admin/Controllers/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Senluo.Spellet.Areas.Admin.Controllers
+{
+    public class ScoreController : AdminController
+    {
+        //
+        // GET: /Admin/Score/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
